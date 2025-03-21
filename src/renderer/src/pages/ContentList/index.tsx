c94a647b74c5aeef3d7f90f508ba31f99d0f7f1b
高宏亮 <2077542643@qq.com>
2025-03-21 16:37:01 +0800

    fix：默认页，样式修改

--- src/renderer/src/pages/ContentList/index.tsx
+++ src/renderer/src/pages/ContentList/index.tsx
@@ -1,8 +1,16 @@
-import { NavLink, Outlet, useLoaderData } from 'react-router-dom'
+import { NavLink, Outlet, useLoaderData, useNavigate } from 'react-router-dom'
 import './contentList.scss'
+import { useEffect } from 'react'
 
 export function ContentList() {
   const contents = useLoaderData() as ContentType[]
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (contents.length) {
+      const content = contents[0]
+      navigate(`/config/category/contentList/${content.category_id}/content/${content.id}`)
+    }
+  }, [])
   return (
     <main className="contentList-page">
       <div className="list">
