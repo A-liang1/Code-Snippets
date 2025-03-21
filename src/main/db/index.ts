72f95aba6b0893b30f931dd6f2ceb936949d0d6b
高宏亮 <2077542643@qq.com>
2025-03-21 10:50:47 +0800

    feat：建表，测试加数据

--- src/main/db/index.ts
+++ src/main/db/index.ts
@@ -1,11 +1,1 @@
-// npm install --save-dev electron-rebuild
-// npx electron-rebuild
-import Database, * as BetterSqlite3 from 'better-sqlite3'
-import { app } from 'electron'
-import { resolve } from 'node:path'
-
-const file = resolve(app.getPath('home'), 'Desktop', 'hd.db')
-const db: BetterSqlite3.Database = new Database(file, {})
-db.pragma('journal_mode = WAL')
-
-export { db }
+import './tables'
